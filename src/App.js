cb68a03af7f26d411f07d702cb37926a5e93e5f7
staskorz <staskorz@gmail.com>
2018-07-25 22:11:55 +0300

    Allowed switching between components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from "react"
 import "./App.css"
 
+import Link from "./components/Link"
+import FirstComponent from "./components/FirstComponent"
+import SecondComponent from "./components/SecondComponent"
+
+const FIRST = "FIRST"
+const SECOND = "SECOND"
+
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      selectedComponent: FIRST,
+    }
+
+    this.setSelectedComponent = this.setSelectedComponent.bind(this)
+    this.onFirstComponentClick = this.onFirstComponentClick.bind(this)
+    this.onSecondComponentClick = this.onSecondComponentClick.bind(this)
+  }
+
+  setSelectedComponent(selectedComponent) {
+    this.setState({ selectedComponent })
+  }
+
+  onFirstComponentClick() {
+    this.setSelectedComponent(FIRST)
+  }
+
+  onSecondComponentClick() {
+    this.setSelectedComponent(SECOND)
+  }
+
   render() {
+    const { selectedComponent } = this.state
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Link label="First Component" onClick={this.onFirstComponentClick} />
+
+          <Link
+            label="Second Component"
+            onClick={this.onSecondComponentClick}
+          />
         </p>
+        {selectedComponent === FIRST ? <FirstComponent /> : null}
+        {selectedComponent === SECOND ? <SecondComponent /> : null}
       </div>
     )
   }
